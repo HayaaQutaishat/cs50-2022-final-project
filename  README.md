60f4ad1c302d6df8f5a4215ea509735603e3b6ed
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-04-11 22:52:59 +0200

    second commit

---
+++  README.md
@@ -0,0 +1,3 @@
+# PROJECT TITLE: Web-based application using HTML, CSS, JavaScript, Python, and SQL
+#### Video Demo:  <URL HERE>
+#### Description: An e-commerce website
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,11 +9,44 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'gfhrjrynsrxc'
 
-db = SQL("sqlite:///users.db")
+db = SQL("sqlite:///project.db")
 
+@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/dress")
+def dress():
+    return render_template("dress.html")
+
+
+@app.route("/skirt")
+def skirt():
+    return render_template("skirt.html")
+
+@app.route("/jeans")
+def jeans():
+    return render_template("jeans.html")
+
+@app.route("/sweater")
+def sweater():
+    return render_template("sweater.html")
+
+
+@app.route("/shoes")
+def shoes():
+    return render_template("shoes.html")
+
+
+@app.route("/top")
+def top():
+    return render_template("top.html")
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -21,58 +54,71 @@ def signup():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        # Ensure email was submitted
-        if not email:
-            return render_template("failure.html", message="Email is required!")
-        
-        # Ensure password was submitted
-        if not password:
-            return render_template("failure.html", message="Password is required!")
-
-        # Ensure confirmation was submitted
-        if not confirmation:
-            return render_template("failure.html", message="Password confirmation is required!")
-
-        # Ensure password matches confirmation
-        if password != confirmation:
-            return render_template("failure.html", message="Password does not match!")
-
-        # Ensure length of password(7 characters min)
-        if len(password) < 7:
-            return render_template("failure.html", message="7 characters minimum!")
 
         #hash password
         hash = generate_password_hash(password)
 
-        # Ensure email is unique or taken???????
+        # Ensure email is unique or taken
         emails = db.execute("SELECT email FROM users")
-        if email in emails:
-            return render_template("failure.html", message="Email already exists!")
-
+        for dict_email in emails:
+            if email == dict_email["email"]:
+                return render_template("failure.html", message="Email already exists!")
     
         # Add email to database
         db.execute("INSERT INTO users (email, hash) VALUES(?, ?)", email, hash)
 
-        # Redirect user to home page
+         # Redirect user to home page
         return redirect("/")
         
     else:
         return render_template("signup.html")
 
 
-# @app.route("/signin", methods=["GET", "POST"])
-# def signin():
-#     if request.method == "POST":
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    return render_template("signin.html")
 
-#     else:
-#         return render_template("signin.html")
+    #return render_template("signin.html")
+    #if request.method == "POST":
+
+        #session.clear()
+        #email = request.form.get("email")
+        #password = request.form.get("password")
+        
+
+    #check if email and pw matches the db, id yess send user to home page, if not send them to failure 
+       # if not email:
+          #  flash("Must provide email!", "warning") 
+            #return redirect(url_for('signin'))
+        #if not password:
+            #flash("Must provide password!", "warning") 
+
+        #rows = db.execute("SELECT * FROM users WHERE id = ?", id)
 
 
-# @app.route("/shop", methods=["GET", "POST"])
-# def shop():
-#     user_id = 
+
+    #else:
+       # return render_template("signin.html")
+
+
+@app.route("/shop", methods=["GET", "POST"])
+def shop():
+    return render_template("shop.html")
+
+
+  
+@app.route("/bag")
+def bag():
+
+    def usd(value):
+        return f"${value:,.2f}"
+
+    #go to session, and check the value of the key"user_id"
+    #user_id = session["user_id"]
+    #rows = db.execute("SELECT item,amount,price FROM cart WHERE user_id = ?", user_id)
+
+    return render_template("bag.html")  
+
 
 
 if __name__ == "__main__":
